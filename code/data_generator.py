c37813d2d10e1bdb4d51fc8b9be28fee1d6e0760
Adarsh Varshney <adarshvarshney@outlook.com>
2023-11-28 15:27:48 +0530

    Modify Hard Negative Sampling Code

--- code/data_generator.py
+++ code/data_generator.py
@@ -17,24 +17,10 @@ def train_generate(datapath, batch_size, few, symbol2id, ent2id, e1rel_e2):
 	# 	print len(train_tasks[query_])
 	# 	if len(train_tasks[query_]) < 4:
 	# 		print len(train_tasks[query_])
-	data = dict()
-	for rel in task_pool:
-		print("\t\tRel: ", rel)
-		data[rel] = dict()
-		for triple in train_tasks[rel]:
-			e1 = triple[0]
-			rel = triple[1]
-			e2 = triple[2]
-			for noise in rel2candidates[rel]:
-				if data[rel].get(e1) is None:
-					data[rel][e1] = dict()
-				if noise not in e1rel_e2[e1+rel] and noise != e2:
-					e1_embed = list(ent_embed[ent2id[e1],:])
-					noise_embed = list(ent_embed[ent2id[noise],:])
-					curr = np.dot(e1_embed, noise_embed)
-					if curr > t:
-						data[rel][e1][noise] = curr
-			# print(data[rel][e1])
+
+	# data contains score of each noise entity for each e1 in each relation
+	# data[rel][e1][noise] = score
+	data = json.load(open(datapath + '/data.json'))
 
 	rel_idx = 0
 	while True:
@@ -49,21 +35,6 @@ def train_generate(datapath, batch_size, few, symbol2id, ent2id, e1rel_e2):
 
 		candidates = rel2candidates[query]
 		#print rel_idx
-
-		if rel_idx % 10000 == 0:
-			t+=0.05
-			print("\t\tThreshold: ", t)
-			for rel in task_pool:
-				for e1 in data[rel].keys():
-					for noise in list(data[rel][e1].keys()):
-						e1_embed = list(ent_embed[ent2id[e1],:])
-						noise_embed = list(ent_embed[ent2id[noise],:])
-						curr = np.dot(e1_embed, noise_embed)
-						if curr > t:
-							data[rel][e1][noise] = curr
-						else:
-							if len(data[rel][e1]) > 1:
-								del data[rel][e1][noise]
 
 		if len(candidates) <= 20:
 			continue
@@ -82,10 +53,16 @@ def train_generate(datapath, batch_size, few, symbol2id, ent2id, e1rel_e2):
 		if len(all_test_triples) == 0:
 			continue
 
+		# Instead of randomly sampling, we use the hardest negative sample
+		# if len(all_test_triples) < batch_size:
+		# 	query_triples = [random.choice(all_test_triples) for _ in range(batch_size)]
+		# else:
+		# 	query_triples = random.sample(all_test_triples, batch_size)
+
 		if len(all_test_triples) < batch_size:
-			query_triples = [random.choice(all_test_triples) for _ in range(batch_size)]
+			query_triples = all_test_triples.extend(random.sample(candidates, batch_size - len(all_test_triples)))
 		else:
-			query_triples = random.sample(all_test_triples, batch_size)
+			query_triples = all_test_triples.sort(key=lambda x: sum(data[query][x[0]]), reverse=True)[:batch_size]
 
 		query_pairs = [[symbol2id[triple[0]], symbol2id[triple[2]]] for triple in query_triples]
 
